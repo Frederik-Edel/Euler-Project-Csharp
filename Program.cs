491bb0b677630df250dce41574d5926f31744bc6
Frederik-Edel <frederikedel@gmail.com>
2022-03-29 16:51:23 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace src
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            for (long i = 2; i < 1000001; i++)
+            {
+                collatz(i);
+                if (chainLen > chainMax)
+                {
+                    chainMax = chainLen;
+                    chainMaxPos = i;
+                }
+                chainLen = 0;
+            }
+            Console.Write(chainMax + ", " + chainMaxPos);
+        }
+        static long chainLen = 0, chainMax = 1, chainMaxPos = 0;
+        static long collatz(long i)
+        {
+            if (i == 1)
+                return 1;
+            chainLen++;
+            if (i % 2 == 0)
+                return collatz(i / 2);
+            if (i % 2 != 0)
+                return collatz(3 * i + 1);
+            return 1;
+        }
+    }
+}
